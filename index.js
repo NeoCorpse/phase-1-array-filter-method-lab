b1aecbdd1e0edc5458ff5b73c9eeb37eaaaab250
Ian Wafula <neocorpse@proton.me>
2025-03-08 11:06:30 +0300

    Add relevant functions to complete lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+function findMatching(drivers, name) {
+  return drivers.filter(
+    (driver) => driver.toLowerCase() === name.toLowerCase()
+  );
+}
+
+function fuzzyMatch(array, str) {
+  return array.filter((item) => item.startsWith(str));
+}
+
+function matchName(array, str) {
+  return array.filter((item) => item.name === str);
+}
